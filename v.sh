667c32f16c95830007ea90be05567fd71aac8768
astolfo671games <minecraft32768@gmail.com>
2026-03-27 20:29:15 +0700

    Fixed error handling

--- v.sh
+++ v.sh
@@ -3,30 +3,24 @@
 clear
 
 # load bytebeat
-
 ./getbb.py
 
 # complile code
-
-gcc -std=c2x -o t thing.c func.c func.h
+gcc -std=c2x -o t thing.c func.c func.h 2> errordata.txt
 gcc -std=c2x -o b bitwcomp.c func.c func.h
-./misc.py
 
 # run code
-
-if [$(cat temp) = ""]
+if [`cat errordata.txt` = ""]
 then
     ./t
     ./b
 else
     echo "Bytebeat is not C-compatible!"
-    rm b && rm temp
+    rm t && rm b
     exit 1
 fi
 
 # delete executables
-
-rm t && rm b && rm temp
-
+rm t && rm b && rm errordata.txt
 read -p ""
 clear
