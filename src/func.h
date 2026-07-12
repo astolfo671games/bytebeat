152716f702009151de065871cda9bb10d331b8c6
astolfo671games <minecraft32768@gmail.com>
2026-07-12 12:48:13 +0700

    Updated func.h

--- src/func.h
+++ src/func.h
@@ -2,8 +2,10 @@
 #define FUNC_H
 
 /*libs*/
-#include <stdio.h>
 #include <ctype.h>
+#include <stdio.h>
+#include <regex.h>
+#include <dirent.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,6 +18,7 @@
 #define SR_OFFSET 24
 #define HEADER_LEN 44
 #define HEADER_DEFAULT "RIFF\xff\xff\xff\xffWAVEfmt\x20\x10\x00\x00\x00\x01\x00\x01\x00\x44\xac\x00\x00\x44\xac\x00\x00\x01\x00\x08\x00\x64\x61ta\xff\xff\xff\xff"
+#define FILENAME_MATCHSTR "^bytebeat_[0-9]{1,16}f_[0-9]{1,16}f[.]wav$"
 #define CONFIG_BUF 256
 #define NUM_NEWLINES 3
 #define FN_MAXLEN 64
@@ -27,6 +30,13 @@ typedef struct ByteData{
 	uint8_t u, v;
 	uint32_t b;
 }ByteData;
+
+typedef struct SongRange{
+	bool is_indir;
+	uint32_t start;
+	uint32_t end;
+	uint32_t len;
+}SongRange;
 
 /*functions*/
 uint32_t get_length(FILE*);
